95c718bb3e8288eb46f01351761a23a6e5be292e
t00m <tomasvirseda@gmail.com>
2022-09-06 22:49:02 +0200

    Add JSON for ISO-3166 Alpha-2 codes and more useful data Add functions for load/save from/to json/dict

---
+++ MiAZ/src/backend.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import glob
+import json
+
+def load_json(filepath: str) -> {}:
+	"""Load into a dictionary a file in json format"""
+	with open(filepath, 'r') as fin:
+		return json.load(fin)
+
+def save_json(filepath: str, adict: {}) -> {}:
+	"""Save dictionary into a file in json format"""
+	with open(filepath, 'w') as fout:
+		json.dump(adict, fout)	
+
+def get_documents(root_dir: str) -> []:
+    """Get documents from a given directory recursively
+    Avoid hidden documents and documents from hidden directories.
+    """
+    documents = set()
+    hidden = set()
+    subdirs = set()
+
+    subdirs.add(os.path.abspath(root_dir))
+    for root, dirs, files in os.walk(os.path.abspath(root_dir)):
+        thisdir = os.path.abspath(root)
+        if os.path.basename(thisdir).startswith('.'):
+            hidden.add(thisdir)
+        else:
+            found = False
+            for hidden_dir in hidden:
+                if hidden_dir in thisdir:
+                    found = True
+            if not found:
+                subdirs.add(thisdir)
+    for directory in subdirs:
+        files = glob.glob(os.path.join(directory, '*'))
+        for thisfile in files:
+            if not os.path.isdir(thisfile):
+                if not os.path.basename(thisfile).startswith('.'):
+                    documents.add(thisfile)
+    return documents
