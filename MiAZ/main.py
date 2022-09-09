9ead0d4b7e7f34b37693b1e6065026ea71723056
Tomás Vírseda <tomasvirseda@gmail.com>
2022-09-09 18:59:39 +0200

    Add argparse options to main program

--- MiAZ/main.py
+++ MiAZ/main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
+import argparse
+
 from MiAZ.backend.env import ENV
 from MiAZ.backend.util import get_version
 from MiAZ.backend.util import desktop_session
@@ -9,14 +12,14 @@ from MiAZ.backend.controller import get_documents
 from MiAZ.backend.controller import valid_filename
 
 
-def main(dirpath: str) -> None:
+def MiAZ(params) -> None:
     print("%s v%s" % (ENV['APP']['shortname'], get_version()))
     if desktop_session() is None:
         print("No Desktop Environment available")
     config = load_config()
     if config is None:
         print("Configuration file not found!")
-    docs = get_documents(dirpath)
+    docs = get_documents(params.SOURCE)
     for doc in docs:
         valid, reasons = valid_filename(doc)
         if not valid:
@@ -25,5 +28,21 @@ def main(dirpath: str) -> None:
                 print ("\t => %s" % reason)
 
 if __name__ == '__main__':
-    import sys
     main(sys.argv[1])
+
+def main():
+    """Set up application arguments and execute."""
+    extra_usage = """"""
+    parser = argparse.ArgumentParser(
+        prog='MiAZ',
+        description='Personal Document Organizer',
+        epilog=extra_usage,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    # MiAZ arguments
+    miaz_options = parser.add_argument_group('MiAZ Options')
+    miaz_options.add_argument('-s', help='Documents source directory', action='store', dest='SOURCE')
+    miaz_options.add_argument('-v', help='Show current version', action='version', version='%s %s' % (ENV['APP']['shortname'], ENV['APP']['version']))
+
+    params = parser.parse_args()
+    MiAZ(params)
