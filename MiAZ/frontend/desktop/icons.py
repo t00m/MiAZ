4c421059492d266389389ac4d461e81b911db911
Tomás Vírseda <tomasvirseda@gmail.com>
2022-09-11 17:48:42 +0200

    Fix caching system for pixbuf mimetype icons

--- MiAZ/frontend/desktop/icons.py
+++ MiAZ/frontend/desktop/icons.py
@@ -37,8 +37,7 @@ class MiAZIconManager(GObject.GObject):
         self.pixbufdict = {}
         self.imgdict = {}
 
-    def get_gicon_from_file_mimetype(self, filepath: str) -> Gio.Icon:
-        mimetype = get_file_mimetype(filepath)
+    def get_gicon_from_file_mimetype(self, mimetype: str) -> Gio.Icon:
         try:
             gicon = self.gicondict[mimetype]
         except KeyError:
@@ -67,16 +66,17 @@ class MiAZIconManager(GObject.GObject):
         return pixbuf
 
     def get_pixbuf_mimetype_from_file(self, filepath, width=48, height=48) -> Pixbuf:
-        key = "%s-%d-%d" % (filepath, width, height)
+        mimetype = get_file_mimetype(filepath)
+        key = "%s-%d-%d" % (mimetype, width, height)
         try:
             pixbuf = self.pixbufdict[key]
         except KeyError:
-            gicon = self.get_gicon_from_file_mimetype(filepath)
+            gicon = self.get_gicon_from_file_mimetype(mimetype)
             paintable = self.get_paintable_from_gicon(gicon)
             gfile = paintable.get_file()
             path = gfile.get_path()
             pixbuf = Pixbuf.new_from_file_at_size(path, width, height)
             self.pixbufdict[key] = pixbuf
-            # ~ print("Cached pixbuf: %s" % pixbuf)
+            print("Cached pixbuf for mimetype: %s" % mimetype)
         return pixbuf
 
