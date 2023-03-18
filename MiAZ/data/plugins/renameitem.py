c775b3867ce8ca0646a361749437006cddb6d71c
Tomás Vírseda <tomasvirseda@gmail.com>
2023-03-18 08:03:43 +0100

    feat(Plugin): Add plugin
    Add a new plugin for renaming a document

---
+++ MiAZ/data/plugins/renameitem.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+# File: export2csv.py
+# Author: Tomás Vírseda
+# License: GPL v3
+# Description: Plugin for exporting items to CSV
+"""
+
+import tempfile
+
+from gi.repository import GObject
+from gi.repository import Peas
+
+from MiAZ.backend.env import ENV
+from MiAZ.backend.log import get_logger
+
+
+class MiAZToolbarRenameItemPlugin(GObject.GObject, Peas.Activatable):
+    __gtype_name__ = 'MiAZToolbarRenameItemPlugin'
+    object = GObject.Property(type=GObject.Object)
+
+    def __init__(self):
+        self.log = get_logger('Plugin.RenameItem')
+
+    def do_activate(self):
+        API = self.object
+        self.app = API.app
+        self.actions = self.app.get_actions()
+        self.backend = self.app.get_backend()
+        self.factory = self.app.get_factory()
+        self.util = self.backend.util
+        self.workspace = API.app.get_widget('workspace')
+        self.workspace.connect("extend-toolbar-top", self.add_toolbar_button)
+        view = self.app.get_widget('workspace-view')
+        selection = view.get_selection()
+        selection.connect('selection-changed', self._on_selection_changed)
+
+    def do_deactivate(self):
+        self.log.debug("Plugin deactivation not implemented")
+        API = self.object
+
+    def _on_selection_changed(self, *args):
+        items = self.workspace.get_selected_items()
+        button = self.app.get_widget('toolbar-top-button-rename')
+        visible = len(items) == 1
+        button.set_visible(visible)
+
+    def add_toolbar_button(self, *args):
+        toolbar_top_right = self.app.get_widget('workspace-toolbar-top-right')
+        button = self.factory.create_button(icon_name='miaz-res-manage', callback=self.callback)
+        button.set_visible(False)
+        self.app.add_widget('toolbar-top-button-rename', button)
+        toolbar_top_right.prepend(button)
+
+    def callback(self, *args):
+        try:
+            item = self.workspace.get_selected_items()[0]
+            self.actions.document_rename_single(item.id)
+        except IndexError:
+            self.log.debug("No item selected")
