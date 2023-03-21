912503282e64fec273ad62a8927990a4ad7d6974
Tomás Vírseda <tomasvirseda@gmail.com>
2023-03-21 23:40:23 +0100

    chore(rpm): scripts/create_rpm.sh install the packages necessary to create a .rpm package

--- scripts/create_rpm.sh
+++ scripts/create_rpm.sh
@@ -1,8 +1,9 @@
 # to be executed from source code root directory
+sudo apt install rpm rpmlint rpm-i18n elfutils
 rm -rf dist
 rm -f *.tar.gz
 mkdir -p logs
-python setup.py bdist_rpm # > logs/rpm.log
+/usr/bin/env python3 setup.py bdist_rpm # > logs/rpm.log
 echo ""
 echo "Install with: sudo rpm -Uhv dist/`ls dist | grep noarch.rpm`"
 echo ""
