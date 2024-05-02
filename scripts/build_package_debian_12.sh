8949253076c87961bf4a7d7f0e2f7a4f664c5a84
Tomás Vírseda <tomasvirseda@gmail.com>
2024-05-02 17:47:12 +0200

    chore: drop support for PIP installation. Using Meson

--- scripts/build_package_debian_12.sh
+++
@@ -1,1 +0,0 @@
-rm -rf MiAZ.egg-info build bdist.linux-x86_64 lib && ./scripts/genbuild.py && /usr/bin/env python3 -m pip install . --user
--- scripts/build_package_delta.sh
+++
@@ -1,1 +0,0 @@
-rm -rf MiAZ.egg-info build bdist.linux-x86_64 lib && /usr/bin/env python3 -m pip install . --user && rm -rf MiAZ.egg-info build bdist.linux-x86_64 lib
