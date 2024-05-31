2d20b759062644884306e73c1db6ec72ea4db977
Tomás Vírseda <tomasvirseda@gmail.com>
2024-06-01 01:06:59 +0200

    wip(Data): add module for data backup/restore

---
+++ MiAZ/backend/data.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+# File: data.py
+# Author: Tomás Vírseda
+# License: GPL v3
+# Description: data backup/restore
+"""
+
+class MiAZData:
+    def __init__(self):
+        pass
+
+class MiAZDataBackup:
+    def __init__(self):
+        pass
+
+class MiAZDataRestore:
+    def __init__(self):
+        pass
