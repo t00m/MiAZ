c97391b3df2b9cff85b5821c79a0ec7d7a1fac45
Tomás Vírseda <tomasvirseda@gmail.com>
2025-03-02 03:56:33 +0100

    set logging level to INFO by default

--- MiAZ/backend/log.py
+++ MiAZ/backend/log.py
@@ -95,7 +95,7 @@ class MiAZLog(logging.getLoggerClass()):
         # Create stream handler for logging to stdout (log all five levels)
         if self.stdout_handler is None:
             self.stdout_handler = logging.StreamHandler(sys.stdout)
-            self.stdout_handler.setLevel(logging.TRACE)
+            self.stdout_handler.setLevel(logging.INFO)
             # ~ self.stdout_handler.setLevel(logging.DEBUG)
             self.stdout_handler.setFormatter(ColorFormatter()) #logging.Formatter("%(levelname)7s | %(lineno)4d  |%(name)-25s | %(asctime)s | %(message)s"))
             self.enable_console_output()
