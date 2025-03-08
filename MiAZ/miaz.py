4a260399f2ff418a05ec3bdc59bc90331b060d12
Tomás Vírseda <tomasvirseda@gmail.com>
2025-03-08 17:53:27 +0100

    feat: Define environment (MiAZ/env.py) at runtime thru Meson

--- MiAZ/miaz.py
+++ MiAZ/miaz.py
@@ -14,15 +14,13 @@ import subprocess
 
 sys.path.insert(1, '@pkgdatadir@')
 
+from MiAZ.env import ENV
 from MiAZ.backend.log import MiAZLog
 from MiAZ.backend.util import which
 
 log = MiAZLog('MiAZ')
-VERSION = '@VERSION@'
-APP_DSC = '@APP_DSC@'
-ENV = {}
 
-# Desktop environment
+# Check Desktop environment
 ENV['DESKTOP'] = {}
 try:
     import gi
@@ -56,77 +54,6 @@ if not ENV['DESKTOP']['ENABLED']:
     log.error("Make sure that Gtk version is >= 4.6 and Adw is >= 1.7")
     sys.exit(-1)
 
-# App
-ENV['APP'] = {}
-ENV['APP']['ID'] = '@APP_ID@'
-ENV['APP']['VERSION'] = VERSION
-ENV['APP']['PGKDATADIR'] = '@pkgdatadir@'
-ENV['APP']['LOCALEDIR'] = '@localedir@'
-ENV['APP']['name'] = "AZ Organizer"
-ENV['APP']['shortname'] = "MiAZ"
-ENV['APP']['description'] = APP_DSC
-ENV['APP']['license'] = 'GPL v3'
-ENV['APP']['license_long'] = "The code is licensed under the terms of the  GPL v3\n\
-                  so you're free to grab, extend, improve and fork the \
-                  code\nas you want"
-ENV['APP']['copyright'] = "Copyright \xa9 2019 Tomás Vírseda"
-ENV['APP']['author'] = 'Tomás Vírseda'
-ENV['APP']['author_email'] = 'tomasvirseda@gmail.com'
-ENV['APP']['author_website'] = 'https://github.com/t00m'
-ENV['APP']['contributors'] = []
-ENV['APP']['website'] = 'https://github.com/t00m/MiAZ'
-
-# Process
-# ~ PID = os.getpid()
-# ~ ENV['PS'] = {}
-# ~ ENV['PS']['PID'] = PID
-# ~ ENV['PS']['NAME'] = open('/proc/%d/comm' % PID, 'r').read().strip()
-
-# Configuration
-ENV['CONF'] = {}
-ENV['CONF']['ROOT'] = ENV['APP']['PGKDATADIR']
-ENV['CONF']['USER_DIR'] = os.path.expanduser('~')
-
-# Local paths
-ENV['LPATH'] = {}
-ENV['LPATH']['ROOT'] = os.path.join(ENV['CONF']['USER_DIR'], f".{ENV['APP']['shortname']}")
-ENV['LPATH']['ETC'] = os.path.join(ENV['LPATH']['ROOT'], 'etc')
-ENV['LPATH']['REPOS'] = os.path.join(ENV['LPATH']['ETC'], 'repos')
-ENV['LPATH']['VAR'] = os.path.join(ENV['LPATH']['ROOT'], 'var')
-ENV['LPATH']['DB'] = os.path.join(ENV['LPATH']['VAR'], 'db')
-ENV['LPATH']['CACHE'] = os.path.join(ENV['LPATH']['VAR'], 'cache')
-ENV['LPATH']['LOG'] = os.path.join(ENV['LPATH']['VAR'], 'log')
-ENV['LPATH']['TMP'] = os.path.join(ENV['LPATH']['VAR'], 'tmp')
-ENV['LPATH']['REPO'] = os.path.join(ENV['LPATH']['TMP'], 'repo')
-ENV['LPATH']['OPT'] = os.path.join(ENV['LPATH']['ROOT'], 'opt')
-ENV['LPATH']['PLUGINS'] = os.path.join(ENV['LPATH']['OPT'], 'plugins')
-ENV['LPATH']['PLUGRES'] = os.path.join(ENV['LPATH']['PLUGINS'], 'resources')
-
-# Global paths
-ENV['GPATH'] = {}
-ENV['GPATH']['ROOT'] = ENV['CONF']['ROOT']
-ENV['GPATH']['DATA'] = os.path.join(ENV['GPATH']['ROOT'], 'resources')
-ENV['GPATH']['DOCS'] = os.path.join(ENV['GPATH']['DATA'], 'docs')
-ENV['GPATH']['ICONS'] = os.path.join(ENV['GPATH']['DATA'], 'icons', 'scalable')
-ENV['GPATH']['FLAGS'] = os.path.join(ENV['GPATH']['ICONS'], 'flags')
-ENV['GPATH']['LOCALE'] = os.path.join(ENV['GPATH']['DATA'], 'po')
-ENV['GPATH']['PLUGINS'] = os.path.join(ENV['GPATH']['DATA'], 'plugins')
-ENV['GPATH']['PLUGRES'] = os.path.join(ENV['GPATH']['PLUGINS'], 'resources')
-ENV['GPATH']['CONF'] = os.path.join(ENV['GPATH']['DATA'], 'conf')
-
-# Common file paths
-ENV['FILE'] = {}
-ENV['FILE']['CONF'] = os.path.join(ENV['LPATH']['ETC'], 'MiAZ-application.json')
-ENV['FILE']['VERSION'] = os.path.join(ENV['GPATH']['DOCS'], 'VERSION')
-ENV['FILE']['APPICON'] = os.path.join(ENV['GPATH']['ICONS'], 'MiAZ.svg')
-ENV['FILE']['LOG'] = os.path.join(ENV['LPATH']['LOG'], 'MiAZ.log')
-ENV['FILE']['GROUPS'] = os.path.join(ENV['LPATH']['ETC'], 'MiAZ-groups.json')
-ENV['FILE']['PURPOSES'] = os.path.join(ENV['LPATH']['ETC'], 'MiAZ-purposes.json')
-ENV['FILE']['CONCEPTS'] = os.path.join(ENV['LPATH']['ETC'], 'MiAZ-concepts.json')
-ENV['FILE']['PEOPLE'] = os.path.join(ENV['LPATH']['ETC'], 'MiAZ-people.json')
-ENV['FILE']['EXTENSIONS'] = os.path.join(ENV['LPATH']['ETC'], 'MiAZ-extensions.json')
-ENV['FILE']['COUNTRIES'] = os.path.join(ENV['LPATH']['ETC'], 'MiAZ-countries.json')
-ENV['FILE']['WHO'] = os.path.join(ENV['LPATH']['ETC'], 'MiAZ-who.json')
 
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 gettext.install('miaz', ENV['APP']['LOCALEDIR'])
@@ -198,8 +125,8 @@ class MiAZ:
         self.log.info(f"{ENV['APP']['shortname']} v{ENV['APP']['VERSION']} - End")
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description=APP_DSC)
-    parser.add_argument('--version', action='version', version=VERSION, help='Show version number and exit.')
+    parser = argparse.ArgumentParser(description=ENV['APP']['description'])
+    parser.add_argument('--version', action='version', version=ENV['APP']['VERSION'], help='Show version number and exit.')
     return parser.parse_args()
 
 
