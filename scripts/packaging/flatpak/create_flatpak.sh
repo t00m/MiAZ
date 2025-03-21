8ffb16096d09c99cf0f949e361c6510d953504c2
Tomás Vírseda <tomasvirseda@gmail.com>
2025-03-21 20:44:13 +0100

    chore: suggest command to run flatpak app after building the package

--- scripts/packaging/flatpak/create_flatpak.sh
+++ scripts/packaging/flatpak/create_flatpak.sh
@@ -1,1 +1,7 @@
 flatpak-builder --force-clean --user --install builddir_flatpak flatpak/io.github.t00m.MiAZ.json
+
+echo "----"
+echo "To run MiAZ app execute:"
+echo " ./scripts/execution/linux/flatpak/exec_flatpak_miaz.sh" 
+echo "----"
+
