3eb52a0c37d1009c8fa2ad963f0e5b94e57b7e4a
Tomás Vírseda <tomasvirseda@gmail.com>
2025-04-10 23:11:01 +0200

    fix: show only Plugins description in Plugin views

--- MiAZ/frontend/desktop/widgets/views.py
+++ MiAZ/frontend/desktop/widgets/views.py
@@ -416,7 +416,7 @@ class MiAZColumnViewPlugin(MiAZColumnViewSelector):
         super().__init__(app, item_type=Plugin)
         self.cv.append_column(self.column_id)
         self.column_id.set_title(_('Plugin Id'))
-        self.column_id.set_visible(True)
+        self.column_id.set_visible(False)
         self.cv.append_column(self.column_title)
         self.column_title.set_title(_('Plugin'))
         self.column_title.set_expand(True)
