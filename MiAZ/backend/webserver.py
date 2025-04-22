1a1ddbbdf8f0f3bfc68ba6fd924b4bc29539367e
Tomás Vírseda <tomasvirseda@gmail.com>
2025-04-22 22:48:06 +0200

    fix: avoid webserver crashes by using different port during startup

--- MiAZ/backend/webserver.py
+++ MiAZ/backend/webserver.py
@@ -2,17 +2,27 @@ import http.server
 import socketserver
 import threading
 import os
+import sys
+import time
+import socket
+import random
 
 from MiAZ.backend.log import MiAZLog
 
 class MiAZHTTPServer:
     def __init__(self, ENV):
-        # ~ super().__init__(host='127.0.0.1', port=8000, directory=ENV['LPATH']['HTML'])
+        self.ENV = ENV
         self.log = MiAZLog('MiAZ.WebServer')
         self.host = '127.0.0.1'
-        self.port = 8000
+        self.port = random.randint(65000, 65535)
         self.directory = ENV['LPATH']['HTML']
         self.httpd = None
+
+    def is_port_in_use(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.settimeout(0.5)  # avoid hanging if something's wrong
+            result = s.connect_ex((self.host, self.port))
+            return result == 0  # 0 means port is open/used
 
     def get_directory(self):
         return self.directory
@@ -27,14 +37,32 @@ class MiAZHTTPServer:
         handler = http.server.SimpleHTTPRequestHandler
         os.chdir(self.directory)  # Serve from this directory
 
+        if self.is_port_in_use():
+            self.port = random.randint(65000, 65535)
+            self.log.warning(f"Previous port was in use. Binding now to port {self.port}")
+            self.start()
+
         try:
             self.httpd = socketserver.TCPServer((self.host, self.port), handler)
             self.thread = threading.Thread(target=self.httpd.serve_forever, daemon=True)
             self.thread.start()
             self.log.info(f"Serving at http://{self.host}:{self.port}/")
-        except OSError:
+        except Exception as error:
+            # When MiAZ is stopped abruptly and a plugin is using
+            # the webserver, it is not possible to reconnect to the same
+            # port again.
+            # Possible solutions:
+            # 1. Wait some seconds and restart the app
+            # 2. Use a random port
+            # Using both approachs for safeguarding connections
+            self.log.error(error)
+            self.log.warning("Sleeping 5 seconds")
+            time.sleep(3)
             self.stop()
-            self.start()
+            python = sys.executable
+            script = self.ENV['APP']['RUNTIME']['EXEC']
+            self.log.info("Application restart: ", [python, script] + sys.argv[1:])
+            os.execv(python, [python, script] + sys.argv[1:])
 
     def stop(self):
         if self.httpd:
