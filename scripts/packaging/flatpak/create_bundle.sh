423383f993ab9767cccbfc3dc6c852d9dd62a484
Tomás Vírseda <tomasvirseda@gmail.com>
2025-04-24 09:45:02 +0200

    chore: create flatpak bundle with the right version

--- scripts/packaging/flatpak/create_bundle.sh
+++ scripts/packaging/flatpak/create_bundle.sh
@@ -1,2 +1,5 @@
+# Extract the version
+PROJECT_VERSION=$(grep -oP "version\s*:\s*'\K[0-9]+\.[0-9]+\.[0-9]+(\+build\.[0-9]+)?" meson.build)
+echo "Building flatpak bundle for MiAZ $PROJECT_VERSION"
 flatpak build-export repo builddir_flatpak
-flatpak build-bundle repo miaz-0.1.9.flatpak io.github.t00m.MiAZ --arch=x86_64
+flatpak build-bundle repo $PROJECT_VERSION.flatpak io.github.t00m.MiAZ --arch=x86_64
