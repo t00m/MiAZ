b0d16fa736fbf13332cbd31fad32f17658d800e9
Tomás Vírseda <tomasvirseda@gmail.com>
2025-06-15 20:26:35 +0200

    fix: do not display MiAZColumnViewMassDelete Id column

--- MiAZ/frontend/desktop/widgets/views.py
+++ MiAZ/frontend/desktop/widgets/views.py
@@ -452,7 +452,7 @@ class MiAZColumnViewMassDelete(MiAZColumnView):
         self.cv.append_column(self.column_id)
         self.column_id.set_title(_('Filename'))
         self.column_id.set_expand(False)
-        self.column_id.set_visible(True)
+        self.column_id.set_visible(False)
         self.cv.append_column(self.column_title)
         self.column_title.set_title(_('Document'))
         self.column_title.set_expand(True)
