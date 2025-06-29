f272a220717545b2f6bb86877fdfef4ec4513179
Tomás Vírseda <tomasvirseda@gmail.com>
2025-06-29 08:27:11 +0200

    fix: import all files, not only those with extension feat: import files asynchronously

--- data/resources/plugins/MiAZImportFromDirectory/adddir.py
+++ data/resources/plugins/MiAZImportFromDirectory/adddir.py
@@ -8,11 +8,14 @@
 import os
 import glob
 from gettext import gettext as _
+import threading
 
+from gi.repository import GLib
 from gi.repository import GObject
 from gi.repository import Peas
 
 from MiAZ.frontend.desktop.services.pluginsystem import MiAZPlugin
+from MiAZ.backend.status import MiAZStatus
 
 plugin_info = {
         'Module':        'adddir',
@@ -65,7 +68,7 @@ class MiAZAddDirectoryPlugin(GObject.GObject, Peas.Activatable):
         if not self.plugin.started():
             # Create menu item for plugin
             mnuItemName = self.plugin.get_menu_item_name()
-            menuitem = self.factory.create_menuitem(name=mnuItemName, label=_('Add documents from directory'), callback=self.import_directory)
+            menuitem = self.factory.create_menuitem(name=mnuItemName, label=_('Add documents from directory'), callback=self.select_directory)
 
             # Add plugin to its default (sub)category
             self.plugin.install_menu_entry(menuitem)
@@ -73,18 +76,49 @@ class MiAZAddDirectoryPlugin(GObject.GObject, Peas.Activatable):
             # Plugin configured
             self.plugin.set_started(started=True)
 
-    def import_directory(self, *args):
+    def select_directory(self, *args):
         self.factory.create_filechooser_for_directories(self._on_filechooser_response)
 
     def _on_filechooser_response(self, dialog, result):
         try:
             folder = dialog.select_folder_finish(result)
-            filepaths = glob.glob(os.path.join(folder, '*.*'))
-            for source in filepaths:
-                btarget = self.util.filename_normalize(source)
-                target = os.path.join(self.repository.docs, btarget)
-                self.util.filename_import(source, target)
-            self.srvdlg.show_info(title='Import directory', body=f'{len(filepaths)} documents imported successfully')
+            filepaths = glob.glob(os.path.join(folder, '*'))
+            self.app.set_status(MiAZStatus.BUSY)
+
+            threading.Thread(target=self.import_directory(filepaths), daemon=True).start()
+
+            # ~ for source in filepaths:
+                # ~ btarget = self.util.filename_normalize(source)
+                # ~ target = os.path.join(self.repository.docs, btarget)
+                # ~ self.util.filename_import(source, target)
+            # ~ self.srvdlg.show_info(title='Import directory', body=f'{len(filepaths)} documents imported successfully')
         except Exception as error:
             self.srvdlg.show_error(title='Error selecting files', body=error)
             self.log.error(f"Error selecting files: {error}")
+            raise
+
+    def import_directory(self, filepaths):
+        total_files = len(filepaths)
+        watcher = self.app.get_service('watcher')
+        watcher.set_active(False)
+
+        for i, filepath in enumerate(filepaths):
+            if hasattr(self, 'cancelled') and self.cancelled:
+                break
+
+            # Update progress
+            progress = (i + 1) / total_files
+            self.log.debug(f"Importing file from {filepath}")
+            GLib.idle_add(self.update_progress, progress, f"Importing file file {i+1}/{total_files}")
+
+            # Import file
+            btarget = self.util.filename_normalize(filepath)
+            target = os.path.join(self.repository.docs, btarget)
+            GLib.idle_add(self.util.filename_import,filepath, target)
+
+    def update_progress(self, fraction, text):
+        self.log.info(f"{fraction} {text}")
+        if fraction >= 1.0:
+            watcher = self.app.get_service('watcher')
+            watcher.set_active(True)
+            self.app.set_status(MiAZStatus.RUNNING)
